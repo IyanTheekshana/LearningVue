76d3470aa7fc5b1957b23f0132737d64ef91a245
Iyan Theekshana <iyan.dev123@gmail.com>
2023-02-27 10:36:34 +0100

    methods

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -8,6 +8,17 @@ const app = Vue.createApp({
       vueLink: "https://vuejs.org/guide/quick-start.html",
     };
   },
+
+  methods: {
+    outPutGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Master Vue!";
+      }
+    },
+  },
 });
 
 app.mount("#user-goal");
